2a5fefccf4a9b25a8118edde45093852cb43e604
Zulte <pochtagar@gmail.com>
2024-02-19 18:06:08 +0100

    tests: added new test scenario with wrong password to login_test.spec.ts

--- tests/login_test.spec.ts
+++ tests/login_test.spec.ts
@@ -18,3 +18,15 @@ test('test login with @positive result', async ({ page }) => {
   await page.getByRole('button', { name: 'Вход' }).click();
   await expect(page.getByText('Вы вошли в систему как «')).toBeVisible();
 });
+
+test('test login with wrong password @negative result', async ({ page }) => {
+  await page.goto('https://turbosms.ua/');
+  await expect(page.getByRole('link', { name: 'Вход' })).toBeVisible();
+  await page.getByRole('link', { name: 'Вход' }).click();
+  await expect(page.getByRole('button', { name: 'Вход' })).toBeVisible();
+  await page.getByPlaceholder('логин или номер телефона').click();
+  await page.getByPlaceholder('логин или номер телефона').fill(username);
+  await page.getByPlaceholder('пароль', { exact: true }).click();
+  await page.getByPlaceholder('пароль', { exact: true }).fill('1111111');
+  await page.getByRole('button', { name: 'Вход' }).click();
+});
--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-
+  await page.goto('https://turbosms.ua/');
+  await expect(page.getByRole('link', { name: 'Вход' })).toBeVisible();
+  await page.getByRole('link', { name: 'Вход' }).click();
+  await expect(page.getByRole('button', { name: 'Вход' })).toBeVisible();
+  await page.getByPlaceholder('логин или номер телефона').click();
+  await page.getByPlaceholder('логин или номер телефона').fill('sssss');
+  await page.getByPlaceholder('пароль', { exact: true }).click();
+  await page.getByPlaceholder('пароль', { exact: true }).fill('1111111');
+  await page.getByRole('button', { name: 'Вход' }).click();
 });
