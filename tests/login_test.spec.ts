227e7a8c47251332f32772be83dc5a60b4180a52
Zulte <pochtagar@gmail.com>
2024-02-19 16:30:22 +0100

    tests:project created and added first test for login

--- tests/login_test.spec.ts
+++ tests/login_test.spec.ts
@@ -1,14 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+const config = require("./config.json");
+
+const username = config.turbo_name;
+const password = config.turbo_pass;
+
+
 test('test login with @positive result', async ({ page }) => {
   await page.goto('https://turbosms.ua/');
   await expect(page.getByRole('link', { name: 'Вход' })).toBeVisible();
   await page.getByRole('link', { name: 'Вход' }).click();
   await expect(page.getByRole('button', { name: 'Вход'})).toBeVisible();
   await page.getByPlaceholder('логин или номер телефона').click();
-  await page.getByPlaceholder('логин или номер телефона').fill('zulte_test');
+  await page.getByPlaceholder('логин или номер телефона').fill(username);
   await page.getByPlaceholder('пароль', { exact: true }).click();
-  await page.getByPlaceholder('пароль', { exact: true }).fill('288731');
+  await page.getByPlaceholder('пароль', { exact: true }).fill(password);
   await page.getByRole('button', { name: 'Вход' }).click();
   await expect(page.getByText('Вы вошли в систему как «')).toBeVisible();
 });
