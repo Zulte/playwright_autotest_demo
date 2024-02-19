a0e2f0cd31929c4dbf8c21ef9c3d366067f0045c
Zulte <pochtagar@gmail.com>
2024-02-19 21:38:23 +0100

    tests: refactoring code in login_test.spec.ts

--- tests/login_test.spec.ts
+++ tests/login_test.spec.ts
@@ -1,32 +1,38 @@
 import { test, expect } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 const config = require("./config.json");
 
 const username = config.turbo_name;
 const password = config.turbo_pass;
 
+export default defineConfig({
+  use: {
+    testIdAttribute: 'id'
+  }
+});
 
-test('test login with @positive result', async ({ page }) => {
+test('test login with @positive result', async ({ page }) => {  
   await page.goto('https://turbosms.ua/');
-  await expect(page.getByRole('link', { name: 'Вход' })).toBeVisible();
+  await expect(page.getByAltText('TurboSMS')).toHaveClass('header__logo')
   await page.getByRole('link', { name: 'Вход' }).click();
-  await expect(page.getByRole('button', { name: 'Вход'})).toBeVisible();
-  await page.getByPlaceholder('логин или номер телефона').click();
-  await page.getByPlaceholder('логин или номер телефона').fill(username);
-  await page.getByPlaceholder('пароль', { exact: true }).click();
-  await page.getByPlaceholder('пароль', { exact: true }).fill(password);
-  await page.getByRole('button', { name: 'Вход' }).click();
+  await expect(page.locator('id=submit_auth')).toBeVisible();
+  await page.locator('id=auth_login').click();
+  await page.locator('id=auth_login').fill(username);
+  await page.locator('id=auth_password').click();
+  await  page.locator('id=auth_password').fill(password);
+  await page.locator('id=submit_auth').click();
   await expect(page.getByText('Вы вошли в систему как «')).toBeVisible();
 });
 
 test('test login with wrong password @negative result', async ({ page }) => {
   await page.goto('https://turbosms.ua/');
-  await expect(page.getByRole('link', { name: 'Вход' })).toBeVisible();
+  await expect(page.getByAltText('TurboSMS')).toHaveClass('header__logo')
   await page.getByRole('link', { name: 'Вход' }).click();
-  await expect(page.getByRole('button', { name: 'Вход' })).toBeVisible();
-  await page.getByPlaceholder('логин или номер телефона').click();
-  await page.getByPlaceholder('логин или номер телефона').fill(username);
-  await page.getByPlaceholder('пароль', { exact: true }).click();
-  await page.getByPlaceholder('пароль', { exact: true }).fill('1111111');
-  await page.getByRole('button', { name: 'Вход' }).click();
+  await expect(page.locator('id=submit_auth')).toBeVisible();
+  await page.locator('id=auth_login').click();
+  await page.locator('id=auth_login').fill(username);
+  await page.locator('id=auth_password').click();
+  await page.locator('id=auth_password').fill('1111111');
+  await page.locator('id=submit_auth').click();
 });
