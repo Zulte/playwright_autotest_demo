4f26302d5065ac2e6f79058370cef43cfd25e1af
Zulte <pochtagar@gmail.com>
2024-02-20 16:43:20 +0100

    tests: created test for editing message

--- tests/send_sms.spec.ts
+++ tests/send_sms.spec.ts
@@ -45,6 +45,43 @@ test('send sms with right data @positive', async ({ page }) => {
   await expect(page.getByText('Сообщение успешно поставлено в очередь отправки')).toBeVisible();
 });
 
+test('user decide to edit message @positive', async ({ page }) => {
+  await page.goto('https://turbosms.ua/');
+  await expect(page.getByAltText('TurboSMS')).toHaveClass('header__logo')
+  await page.getByRole('link', { name: 'Вход' }).click();
+  await expect(page.locator('id=submit_auth')).toBeVisible();
+  await page.locator('id=auth_login').click();
+  await page.locator('id=auth_login').fill(username);
+  await page.locator('id=auth_password').click();
+  await page.locator('id=auth_password').fill(password);
+  await page.locator('id=submit_auth').click();
+  await expect(page.getByText('Вы вошли в систему как «')).toBeVisible();
+  await page.getByRole('link', { name: 'Отправить SMS' }).click();
+  await page.locator('id=single_text').click();
+  await page.locator('id=single_text').pressSequentially('test_test_test_test_test_test_test');
+  await expect(page.getByRole('heading', { name: 'Отправка SMS' })).toBeVisible();
+  await expect(page.getByText('34', { exact: true })).toBeVisible();
+  await expect(page.getByText('126', { exact: true })).toBeVisible();
+  await expect(page.getByRole('strong')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'TAXI' })).toBeVisible();
+  await page.getByRole('link', { name: 'TAXI' }).click();
+  await page.locator('#select2-result-label-9').click();
+  await expect(page.getByRole('link', { name: 'MAGAZIN' })).toBeVisible();
+  await expect(page.getByText('Номера получателей в международном формате, например 380672316939(не более 3')).toBeVisible();
+  await page.getByText('Номера получателей в международном формате, например 380672316939(не более 3').click();
+  await page.getByLabel('Номера получателей в международном формате, например 380672316939(не более 3').fill('380666118336');
+  await page.locator('id=submitter_single').click();
+  await expect(page.getByText('Время отправки: Немедленно')).toBeVisible();
+  await expect(page.getByText('Отправитель: MAGAZIN')).toBeVisible();
+  await expect(page.getByText('Номер получателя: 380666118336')).toBeVisible();
+  await expect(page.getByText('Количество номеров: 1')).toBeVisible();
+  await expect(page.getByText('Текст сообщения: test_test_test_test_test_test_test')).toBeVisible();
+  await expect(page.getByText('Размер SMS: 1 сегм')).toBeVisible();
+  await expect(page.getByText('Стоимость 1 SMS: 0.66грн')).toBeVisible();
+  await expect(page.getByText('Суммарная стоимость: 0.66грн')).toBeVisible();
+  await page.getByRole('button', { name: 'Редактировать данные' }).click();
+  await expect(page.getByRole('heading', { name: 'Отправка SMS' })).toBeVisible();
+});
 
 test('send sms without number or with wrong number @negative', async ({ page }) => {
   await page.goto('https://turbosms.ua/');
