49fc4a4f33fd86e9002be552e0c7414d6c5f6ff9
Zulte <pochtagar@gmail.com>
2024-02-20 16:38:13 +0100

    tests: added test for send with wrong number send_sms.spec.ts

--- tests/send_sms.spec.ts
+++ tests/send_sms.spec.ts
@@ -44,3 +44,33 @@ test('send sms with right data @positive', async ({ page }) => {
   await page.locator('id=submitter_single').click();
   await expect(page.getByText('Сообщение успешно поставлено в очередь отправки')).toBeVisible();
 });
+
+
+test('send sms without number or with wrong number @negative', async ({ page }) => {
+  await page.goto('https://turbosms.ua/');
+  await expect(page.getByAltText('TurboSMS')).toHaveClass('header__logo')
+  await page.getByRole('link', { name: 'Вход' }).click();
+  await expect(page.locator('id=submit_auth')).toBeVisible();
+  await page.locator('id=auth_login').click();
+  await page.locator('id=auth_login').fill(username);
+  await page.locator('id=auth_password').click();
+  await page.locator('id=auth_password').fill(password);
+  await page.locator('id=submit_auth').click();
+  await expect(page.getByText('Вы вошли в систему как «')).toBeVisible();
+  await page.getByRole('link', { name: 'Отправить SMS' }).click();
+  await page.locator('id=single_text').click();
+  await page.locator('id=single_text').pressSequentially('test_test_test_test_test_test_test');
+  await expect(page.getByRole('heading', { name: 'Отправка SMS' })).toBeVisible();
+  await expect(page.getByText('34', { exact: true })).toBeVisible();
+  await expect(page.getByText('126', { exact: true })).toBeVisible();
+  await expect(page.getByRole('strong')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'TAXI' })).toBeVisible();
+  await page.getByRole('link', { name: 'TAXI' }).click();
+  await page.locator('#select2-result-label-9').click();
+  await expect(page.getByRole('link', { name: 'MAGAZIN' })).toBeVisible();
+  await expect(page.getByText('Номера получателей в международном формате, например 380672316939(не более 3')).toBeVisible();
+  await page.getByText('Номера получателей в международном формате, например 380672316939(не более 3').click();
+  await page.getByLabel('Номера получателей в международном формате, например 380672316939(не более 3').fill('38093');
+  await page.getByRole('button', { name: 'Просмотреть сообщение' }).click();
+  await expect(page.getByText('Введите правильный номер получателя в международном формате (только цифры)')).toBeVisible();
+});
